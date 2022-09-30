6aa7dfd37c7d7de86666d1c1dfa31ebe14b9eefe
Margarita P <rprivalko@gmail.com>
2022-09-30 19:25:11 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+type PropsType = {
+    completed: boolean,
+    id: number,
+    title: string,
+    userId: number
+}
 
 function App() {
+
+    const [todos, setTodos] = useState<PropsType[]>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(json => setTodos(json))
+    }, [])
+
+    console.log(todos)
+    const onClickHandler = () => {
+        setTodos([]);
+    }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={onClickHandler}>Clean Posts</button>
+        <ul>
+            {todos.map(el => {
+                return(
+                    <li>
+                        <span>{el.id} - </span>
+                        <span>{el.title}</span>
+                        <span>{el.completed}</span>
+                    </li>
+                )
+            })
+            }
+
+        </ul>
+
+
     </div>
   );
 }
